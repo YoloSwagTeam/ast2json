452a0410bd3d9a25d985e393505d82a61df6328b
Laurent Peuch <cortex@worlddomination.be>
2013-03-03 07:48:45 +0100

    [mod] rename ast_to_json to ast2json, more intuitive

--- ast2json.py
+++ ast2json.py
@@ -28,7 +28,7 @@ from _ast import AST
 from ast import parse
 
 
-def ast_to_json(node):
+def ast2json(node):
     to_return = {}
     to_return['_type'] = node.__class__.__name__
     for attr in dir(node):
@@ -45,12 +45,12 @@ def get_value(attr_value):
     if isinstance(attr_value, (int, basestring)):
         return attr_value
     if isinstance(attr_value, list):
-        return [ast_to_json(x) for x in attr_value]
+        return [ast2json(x) for x in attr_value]
     if isinstance(attr_value, AST):
-        return ast_to_json(attr_value)
+        return ast2json(attr_value)
     print attr_value
 
 
 if __name__ == '__main__':
     import json
-    print json.dumps(ast_to_json(parse(open(__file__, "r").read())), indent=4)
+    print json.dumps(ast2json(parse(open(__file__, "r").read())), indent=4)
