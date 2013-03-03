1f077473f6b5ced4468dc4923dd4d193c5554b1b
Laurent Peuch <cortex@worlddomination.be>
2013-03-03 07:45:47 +0100

    [enh] add a setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from setuptools import setup
+
+setup(name='ast2json',
+      version='0.1',
+      description='convert a python source code into json-dumpable data (dict and lists with strings, ints, ...)',
+      author='Laurent Peuch',
+      #long_description='',
+      author_email='cortex@worlddomination.be',
+      url='https://github.com/Psycojoker/ast2json',
+      py_modules=['ast2json'],
+      license='BSD',
+      keywords='ast',
+     )
