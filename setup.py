59386e278a028fe373535b9f505db40cdb24f8fb
Laurent Peuch <cortex@worlddomination.be>
2016-09-03 19:33:03 +0200

    [enh] release 0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='ast2json',
-      version='0.1',
+      version='0.2',
       description='convert a python source code into json-dumpable data (dict and lists with strings, ints, ...)',
       author='Laurent Peuch',
       long_description=open('README.rst', 'r').read(),
