e17cbdb57a6d7ec0d3d1f145093ca0d32f8a56c1
Laurent Peuch <cortex@worlddomination.be>
2016-11-13 03:43:11 +0100

    [mod] remove useless comments

--- setup.py
+++ setup.py
@@ -29,21 +29,16 @@ setup(name='ast2json',
                    #   4 - Beta
                    #   5 - Production/Stable
                    'Development Status :: 3 - Alpha',
-                   
+
                    # Indicate who your project is intended for
                    'Intended Audience :: Developers',
-                   
+
                    # Pick your license as you wish (should match "license" above)
                    #'License :: OSI Approved :: MIT License',
-                   
+
                    # Specify the Python versions you support here. In particular, ensure
                    # that you indicate whether you support Python 2, Python 3 or both.
                    'Programming Language :: Python :: 2',
-                   #'Programming Language :: Python :: 2.6',
-                   #'Programming Language :: Python :: 2.7',
                    'Programming Language :: Python :: 3',
-                   #'Programming Language :: Python :: 3.3',
-                   #'Programming Language :: Python :: 3.4',
-                   #'Programming Language :: Python :: 3.5',
                    ],
      )
